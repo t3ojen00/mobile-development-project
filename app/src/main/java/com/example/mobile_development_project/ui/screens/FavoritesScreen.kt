5aa4673819c7af85454bf7d997ea972a882258f9
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-19 13:49:38 +0200

    Added default screen components

--- app/src/main/java/com/example/mobile_development_project/ui/screens/FavoritesScreen.kt
+++ app/src/main/java/com/example/mobile_development_project/ui/screens/FavoritesScreen.kt
@@ -1,2 +1,9 @@
 package com.example.mobile_development_project.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoritesScreen() {
+    Text("Favorite locations screen")
+}
