aeb09c3d4d087b004567c69a283d749f66b3c04b
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-20 17:19:44 +0200

    Refactored  scaffold layout from MainActivity to ProjectApp composable

--- app/src/main/java/com/example/mobile_development_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_development_project/MainActivity.kt
@@ -5,14 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import org.osmdroid.config.Configuration
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.example.mobile_development_project.navigation.Navigation
-import com.example.mobile_development_project.ui.components.navigation.BottomBar
-import com.example.mobile_development_project.ui.components.navigation.TopBar
 import com.example.mobile_development_project.ui.theme.MobiledevelopmentprojectTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,16 +15,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MobiledevelopmentprojectTheme {
-                val navController = rememberNavController()
-                Scaffold(
-                    topBar = { TopBar(navController = navController) },
-                    bottomBar = { BottomBar(navController)},
-                    modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Navigation(
-                        navController = navController,
-                        modifier = Modifier.padding(innerPadding))
-
-                }
+                ProjectApp()
             }
         }
     }
