02b628d85b4bd1011fe2f36e8b30bb1291ee4956
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-24 10:24:30 +0200

    AddLocation screen draft

--- app/src/main/java/com/example/mobile_development_project/ui/screens/AddLocationScreen.kt
+++ app/src/main/java/com/example/mobile_development_project/ui/screens/AddLocationScreen.kt
@@ -3,30 +3,24 @@ package com.example.mobile_development_project.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.mobile_development_project.ui.components.reusable.CardFormComponent
 import com.example.mobile_development_project.viewModels.AddLocationViewModel
 
 @Composable
 fun AddLocationScreen(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier.padding(16.dp),
     viewModel: AddLocationViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
     Column(
         modifier
         .fillMaxSize()
-        .padding(16.dp)
     ) {
         Text("Add location screen")
-        Card( modifier = Modifier
-            .fillMaxSize()
-        ) {
-            Text("test")
-
-        }
+        CardFormComponent()
     }
 
 }
