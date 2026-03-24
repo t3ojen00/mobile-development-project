2fd07b6c683ac11be308e6a456c01fe9350ed7b0
qusterrr <t3page00@students.oamk.fi>
2026-03-24 17:41:12 +0200

    Add and update model classes

---
+++ app/src/main/java/com/example/mobile_development_project/data/models/Favourite.kt
@@ -0,0 +1,8 @@
+package com.example.mobile_development_project.data.models
+
+data class Favourite(
+    val id: String = "",
+    val userId: String = "",
+    val locationId: String = "",
+    val createdAt: Long? = null
+)
