d68276dd858dade721cce96b095c91ea5bea25be
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-26 12:55:03 +0200

    Added button to call saveLocation method

--- app/src/main/java/com/example/mobile_development_project/ui/screens/AddLocationScreen.kt
+++ app/src/main/java/com/example/mobile_development_project/ui/screens/AddLocationScreen.kt
@@ -1,12 +1,16 @@
 package com.example.mobile_development_project.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.mobile_development_project.ui.components.reusable.CardFormComponent
+import com.example.mobile_development_project.ui.components.reusable.PrimaryButton
 import com.example.mobile_development_project.viewModels.LocationViewModel
 
 @Composable
@@ -20,6 +24,13 @@ fun AddLocationScreen(
         .padding(16.dp)
     ) {
         CardFormComponent(viewModel = viewModel)
-    }
+        Spacer(modifier = Modifier.height(16.dp))
 
+        // save button
+        PrimaryButton(
+            label = "Save location",
+            onClick = { viewModel.saveLocation() },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
