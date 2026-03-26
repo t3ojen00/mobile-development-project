11c16127b6ee8b76231fffdc0e87c2cb57899c49
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-26 12:53:51 +0200

    Initialized Firebase connection and added saveLocation method

--- app/src/main/java/com/example/mobile_development_project/viewModels/LocationViewModel.kt
+++ app/src/main/java/com/example/mobile_development_project/viewModels/LocationViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LocationViewModel : ViewModel() {
+
+    val db = FirebaseFirestore.getInstance()
 
     var locationName by mutableStateOf("")
         private set
@@ -24,6 +27,25 @@ class LocationViewModel : ViewModel() {
     }
     fun onDescriptionChange(value: String) {
         description = value
+    }
+    // saves location to firebase
+    fun saveLocation() {
+        val location = hashMapOf(
+            "name" to locationName,
+            "description" to description,
+            "tags" to tags,
+            "lat" to gpsCoordinates?.first,
+            "lng" to gpsCoordinates?.second
+        )
+
+        db.collection("locations")
+            .add(location)
+            .addOnSuccessListener {
+                println("Saved successfully")
+            }
+            .addOnFailureListener {
+                println("Error: ${it.message}")
+            }
     }
 
     // logic for tags
