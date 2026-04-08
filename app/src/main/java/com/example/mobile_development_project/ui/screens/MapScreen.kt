e5138d81ef8ad7718ac9fa337da15bd87a1ca0a5
Anniina-55 <anniina.leikas55@gmail.com>
2026-04-08 21:09:45 +0300

    Updated map screen to render all approved locations

--- app/src/main/java/com/example/mobile_development_project/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/mobile_development_project/ui/screens/MapScreen.kt
@@ -4,54 +4,69 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.mobile_development_project.R
 import com.example.mobile_development_project.navigation.NavRoutes
 import com.example.mobile_development_project.ui.createMapMarker
+import com.example.mobile_development_project.viewModels.MapViewModel
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 
 @Composable
-fun MapScreen(navController: NavHostController) {
+fun MapScreen(
+    viewModel: MapViewModel = viewModel(),
+    navController: NavHostController
+) {
     val context = LocalContext.current
     // custom marker
     val markerColor = MaterialTheme.colorScheme.primary.toArgb()
     val scaledMarker = createMapMarker(context, R.drawable.location_marker, 120, markerColor)
+
+    // fetch all approved locations from database
+    LaunchedEffect(Unit) {
+        viewModel.fetchApprovedLocations()
+    }
+
+    val approvedLocations = viewModel.approvedLocations
 
     val mapView = remember {
         MapView(context).apply {
             setTileSource(TileSourceFactory.MAPNIK)
             setMultiTouchControls(true)
 
-            val startPoint = GeoPoint(65.0051, 25.2819)
+            val startPoint = GeoPoint(65.0099, 25.4730)
             controller.setZoom(12.0)
             controller.setCenter(startPoint)
+        }
+    }
 
-            //test marker to see that the locationdetailscreen works (it doesnt render because mock id doesnt exist in firebase)
-           val marker = Marker(this).apply {
-                position = startPoint
+    LaunchedEffect(approvedLocations) {
+        mapView.overlays.clear()
+        approvedLocations.forEach { location ->
+            val marker = Marker(mapView).apply {
+                position = GeoPoint(location.latitude, location.longitude)
                 setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                title = "Test Location"
+                title = location.name
                 icon = scaledMarker
-
                 setOnMarkerClickListener { _, _ ->
                     navController.navigate(
-                        NavRoutes.LocationDetail.replace("{id}", "test123")
+                        NavRoutes.LocationDetail.replace("{id}", location.id)
                     )
                     true
                 }
             }
-
-            overlays.add(marker)
-            invalidate()
+            mapView.overlays.add(marker)
         }
+        mapView.invalidate()
     }
 
 
