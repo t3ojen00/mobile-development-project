850e6275f6f0cf2fbfa16bfae059ad0eaf61543d
Anniina-55 <anniina.leikas55@gmail.com>
2026-04-16 22:43:07 +0300

    Added edit-menu and conditional component rendering based on add/edit mode

--- app/src/main/java/com/example/mobile_development_project/ui/components/reusable/CardForm.kt
+++ app/src/main/java/com/example/mobile_development_project/ui/components/reusable/CardForm.kt
@@ -34,9 +34,13 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 import android.Manifest
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -44,10 +48,16 @@ import androidx.navigation.NavHostController
 import com.example.mobile_development_project.data.models.ErrorCause
 import com.example.mobile_development_project.navigation.NavRoutes
 
+enum class FormMode {
+    ADD,
+    EDIT
+}
+
 @Composable
 fun CardFormComponent(
     viewModel: AddLocationViewModel,
-    navController: NavHostController
+    navController: NavHostController,
+    mode: FormMode = FormMode.ADD,
 ) {
 
     val focusManager = LocalFocusManager.current
@@ -68,13 +78,19 @@ fun CardFormComponent(
     val permissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
     ) { granted ->
-        if (granted) {
+        if (!granted) {
+            viewModel.setError(ErrorCause.LOCATION_PERMISSION_DENIED)
+            return@rememberLauncherForActivityResult
+        }
             isLoading = true
+
             val cancellationToken = CancellationTokenSource().token
             val client = LocationServices.getFusedLocationProviderClient(context)
-            client.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, cancellationToken)
-                .addOnSuccessListener { location ->
-                    isLoading = false
+
+                client.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, cancellationToken)
+                    .addOnSuccessListener { location ->
+                        Log.d("GPS", "SOURCE LAT=${location.latitude}, LNG=${location.longitude}")
+                        isLoading = false
                         if (location != null) {
                             viewModel.setGpsCoordinates(location.latitude, location.longitude)
                         } else {
@@ -82,15 +98,12 @@ fun CardFormComponent(
                             viewModel.setError(ErrorCause.LOCATION_UNAVAILABLE)
                         }
                     }
-                // whole call fails
-                .addOnFailureListener {
-                    viewModel.setError(ErrorCause.LOCATION_FETCH_FAILED)
-                }
-
-        } else {
-            viewModel.setError(ErrorCause.LOCATION_PERMISSION_DENIED)
-        }
-    }
+                    // whole call fails
+                    .addOnFailureListener {
+                        viewModel.setError(ErrorCause.LOCATION_FETCH_FAILED, exception = it)
+                        isLoading = false
+                    }
+      }
 
     // list of images (uses Image model)
     val initialImages = listOf(
@@ -106,14 +119,24 @@ fun CardFormComponent(
             .fillMaxWidth()
             .wrapContentHeight(),
         shape = CardDefaults.elevatedShape,
-        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         colors = CardDefaults.cardColors(containerColor = cardColor)
     ) {
 
         Column(modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 24.dp, horizontal = 18.dp)
+            .padding(vertical = 18.dp, horizontal = 18.dp)
         ) {
+
+            if (mode == FormMode.EDIT) {
+                ImageCarousel(
+                    items = images,
+                    editMode = true,
+                    onRemoveImage = {},
+                    onAddImage = {}
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
 
             TextFieldComponent(
                 value = viewModel.locationName,
@@ -122,8 +145,12 @@ fun CardFormComponent(
                 maxCharacters = 50,
                 maxLines = 1,
                 focusManager = focusManager,
-                modifier = Modifier.fillMaxWidth(),
-                )
+                trailingIcon =
+                    if (mode == FormMode.EDIT) {
+                    { Icon(Icons.Default.Edit, contentDescription = "Edit") }
+                    } else null,
+                modifier = Modifier.fillMaxWidth()
+            )
             Spacer(modifier = Modifier.height(12.dp))
 
             TextFieldComponent(
@@ -133,6 +160,11 @@ fun CardFormComponent(
                 maxCharacters = 130,
                 maxLines = 4,
                 focusManager = focusManager,
+                trailingIcon =
+                    if (mode == FormMode.EDIT) {
+                        { Icon(
+                            Icons.Default.Edit, contentDescription = "Edit") }
+                    } else null,
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight(),
@@ -196,51 +228,51 @@ fun CardFormComponent(
                     Spacer(modifier = Modifier.height(12.dp)) // vertical space between tags
                 }
             }
-            Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(6.dp))
 
             Row( modifier = Modifier.fillMaxWidth()) {
+                if(mode == FormMode.ADD) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .wrapContentSize(Alignment.TopStart)
+                    ) {
+                        PrimaryButton(
+                            label = "Select location",
+                            onClick = { expanded = true },
+                            modifier = Modifier.fillMaxWidth(),
+                        )
 
-                Box(modifier = Modifier
-                    .weight(1f)
-                    .wrapContentSize(Alignment.TopStart)
-                ) {
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false },
+                            Modifier.padding(4.dp)
+                        ) {
+                            DropdownMenuItem(
+                                text = { Text("Select from map") },
+                                onClick = {
+                                    expanded = false
+                                    navController.navigate(NavRoutes.SelectFromMap)
+                                }
+                            )
+
+                            DropdownMenuItem(
+                                text = { Text("Allow access to GPS") },
+                                onClick = {
+                                    expanded = false
+                                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                                }
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
 
                     PrimaryButton(
-                        label = "Select location",
-                        onClick = { expanded = true },
-                        modifier = Modifier.fillMaxWidth(),
-                        )
-
-                    DropdownMenu(
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false },
-                        Modifier.padding(4.dp)
-                    ) {
-                        DropdownMenuItem(
-                            text = { Text("Select from map") },
-                            onClick = {
-                                expanded = false
-                                navController.navigate(NavRoutes.SelectFromMap)
-                            }
-                        )
-
-                        DropdownMenuItem(
-                            text = { Text("Allow access to GPS") },
-                            onClick = {
-                                expanded = false
-                                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                            }
-                        )
-                    }
+                        label = "Add images",
+                        onClick = { },
+                        modifier = Modifier.weight(1f)
+                    )
                 }
-
-                Spacer(modifier = Modifier.width(16.dp))
-
-                PrimaryButton(
-                    label = "Add images",
-                    onClick = { },
-                    modifier = Modifier.weight(1f)
-                )
             }
             if (isLoading) {
                 Box(
@@ -254,9 +286,17 @@ fun CardFormComponent(
                     )
                 }
             }
-            Spacer(modifier = Modifier.height(20.dp))
 
-            ImageCarousel(items = images)
+            if (mode == FormMode.ADD) {
+                Spacer(modifier = Modifier.height(20.dp))
+                ImageCarousel(
+                    items = images,
+                    editMode = false,
+                    onRemoveImage = {},
+                    onAddImage = {}
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
 
         }
     }
