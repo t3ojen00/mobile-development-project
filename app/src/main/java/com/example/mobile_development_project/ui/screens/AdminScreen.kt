29746baed94f35240328490236418a58cf7e2e2d
Anniina-55 <anniina.leikas55@gmail.com>
2026-04-25 18:46:40 +0300

    Added success and confirmation dialogs

--- app/src/main/java/com/example/mobile_development_project/ui/screens/AdminScreen.kt
+++ app/src/main/java/com/example/mobile_development_project/ui/screens/AdminScreen.kt
@@ -8,32 +8,47 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.mobile_development_project.data.models.MsgType
+import com.example.mobile_development_project.data.models.User
 import com.example.mobile_development_project.ui.theme.Burgundy
 import com.example.mobile_development_project.viewModels.AdminViewModel
 import com.example.mobile_development_project.ui.components.admin_view.PendingLocations
 import com.example.mobile_development_project.ui.components.admin_view.RejectedLocations
 import com.example.mobile_development_project.ui.components.admin_view.UsersList
 import com.example.mobile_development_project.ui.components.reusable.ScrollToTopButton
+import com.example.mobile_development_project.ui.theme.Attention
+import com.example.mobile_development_project.ui.theme.OrangeAccent
 import kotlinx.coroutines.launch
 
-
+sealed class Dialog {
+    data object None : Dialog()
+    data class DeleteUser(val user: User) : Dialog()
+    data class DeleteLocation(val locationId: String) : Dialog()
+}
 @Composable
 fun AdminScreen(
     viewModel: AdminViewModel = viewModel(),
@@ -51,6 +66,8 @@ fun AdminScreen(
     val pendingState = rememberLazyListState()
     val usersState = rememberLazyListState()
     val rejectedState = rememberLazyListState()
+    var dialogState by remember { mutableStateOf<Dialog>(Dialog.None) }
+    val message = viewModel.uiMessage
 
     Box(modifier = Modifier.fillMaxSize()) {
 
@@ -101,7 +118,7 @@ fun AdminScreen(
                         users = viewModel.allUsers,
                         viewModel = viewModel,
                         onDelete = { user ->
-                            viewModel.deleteUser(user.id)
+                            dialogState = Dialog.DeleteUser(user)
                         },
                         onPromote = { user, role ->
                             viewModel.updateUserRole(user.id, role)
@@ -113,7 +130,10 @@ fun AdminScreen(
                     2 -> RejectedLocations(
                         viewModel,
                         navController,
-                        listState = rejectedState
+                        listState = rejectedState,
+                        onDelete = { location ->
+                            dialogState = Dialog.DeleteLocation(location.id)
+                        }
                     )
                     /*
                     3 -> Statistics()
@@ -142,7 +162,90 @@ fun AdminScreen(
                 .padding(12.dp)
         )
     }
-}
+    if (message?.second == MsgType.SUCCESS) {
+        AlertDialog(
+            onDismissRequest = { viewModel.clearMessage() },
+            confirmButton = {
+                TextButton(
+                    onClick = { viewModel.clearMessage() }
+                ) {
+                    Text("OK", color = OrangeAccent)
+                }
+            },
+            title = { Text("Success") },
+            text = { Text(message.first) }
+        )
+    }
+    when (dialogState) {
 
+        is Dialog.DeleteUser -> {
+            val user = (dialogState as Dialog.DeleteUser).user
+
+            AlertDialog(
+                onDismissRequest = { dialogState = Dialog.None },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            viewModel.deleteUser(user.id)
+                            dialogState = Dialog.None
+                        }
+                    ) {
+                        Text(
+                            "Delete",
+                            color = Attention)
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = { dialogState = Dialog.None }
+                    ) {
+                        Text(
+                            "Cancel",
+                        color = OrangeAccent)
+                    }
+                },
+                title = {
+                    Text("Delete user",
+                    color = Attention)
+                        },
+                text = {
+                    Text(
+                        buildAnnotatedString {
+                            append("Are you sure you want to delete ")
+                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                append(user.username)
+                            }
+                            append("?")
+                        }
+                    )
+                }
+            )
+        }
+        is Dialog.DeleteLocation -> {
+            val locationId = (dialogState as Dialog.DeleteLocation).locationId
+            AlertDialog(
+                onDismissRequest = { dialogState = Dialog.None },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            viewModel.deleteLocation(locationId)
+                            dialogState = Dialog.None
+                        })
+                    { Text("Delete", color = Attention) }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = { dialogState = Dialog.None }
+                    ) { Text("Cancel", color = OrangeAccent) }
+                },
+                title = {
+                    Text("Delete location", color = Attention) },
+                text = {
+                    Text("Are you sure you want to delete this location?") }
+            )
+        }
+        Dialog.None -> Unit
+    }
+}
 
 
